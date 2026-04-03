21583ec2e222240b178184edae1ff58198c9c4fa
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-04-03 11:22:26 +0530

    Add PORT On index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const url = process.env.MONGO_URL;
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
