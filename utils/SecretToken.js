40e807f4f135885571f488778edc6c45fe139197
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-05-06 23:44:41 +0530

    feat: add some change

---
+++ utils/SecretToken.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+
+module.exports.createSecretToken = (id) => {
+  return jwt.sign(
+    { id },
+    process.env.TOKEN_KEY,
+    {
+      expiresIn: "3d",  
+    }
+  );
+};
