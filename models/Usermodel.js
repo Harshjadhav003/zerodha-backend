472866e43ad1236501441c9b0e05ade274e8618b
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-06-17 00:01:59 +0530

    fix(user): improve user schema and password security

--- models/Usermodel.js
+++ models/Usermodel.js
@@ -6,15 +6,19 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Your email address is required"],
     unique: true,
+    lowercase: true,
+    trim: true,
   },
   username: {
     type: String,
     required: [true, "Your username is required"],
+    trim: true,
   },
   password: {
     type: String,
     required: [true, "Your password is required"],
-    select: false, //  important
+    select: false,
+    minlength: 6,
   },
   createdAt: {
     type: Date,
@@ -22,10 +26,17 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
+
+// Compare password method
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
